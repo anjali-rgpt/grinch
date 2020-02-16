41290de79a09b86dd4ed3353fa73987494aed03a
AnjaliRagupathi <anjaliragupathi99@gmail.com>
2020-02-16 18:34:39 +0530

    idk what i am doing part 1

---
+++ src/components/Field.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let question, points, option1, option2, option3, option4, correct;
+</script>
+
+<p>{question}</p>
+<input type="checkbox">{option1}
+<input type="checkbox">{option2}
+<input type="checkbox">{option3}
+<input type="checkbox">{option4}
