b702e361113e8115ff9f7291c7e7da246f5477eb
AnjaliRagupathi <anjaliragupathi99@gmail.com>
2020-02-24 00:06:25 +0530

    Changed points marking

--- src/components/Addquestion.svelte
+++ src/components/Addquestion.svelte
@@ -8,7 +8,7 @@
     else if(correct != optiona && correct!=optionb && correct != optionc && correct != optiond){
       alert("Correct answer doesn't match any of the options");
     }
-    else if(points>4.0 || points<1.0){
+    else if(points>10.0 || points<1.0){
       alert("Invalid points.");
     }
     else{
