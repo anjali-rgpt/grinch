784b7551b2f305901bb0e584453641cc73d4ff05
AnjaliRagupathi <anjaliragupathi99@gmail.com>
2020-03-07 23:41:34 +0530

    added admincourse page with database sem data

---
+++ src/routes/adminCourse.svelte
@@ -0,0 +1,31 @@
+<script>
+
+let semesters=[]
+
+onMount(async() => {
+    await db.collection('course-by-sem').get()
+    .then(doc =>{
+        semesters=[]
+        //id=[]
+        doc.array.forEach(element => {
+
+            semesters.push(element.data())
+            semesters=semesters
+            
+        });
+
+        semesters=semesters
+    });
+}
+)
+</script>
+
+
+<svelte:head>
+	<title>Admin: Add Course</title>
+</svelte:head>
+
+<select name="semester">
+<option></option>
+</select>
+
