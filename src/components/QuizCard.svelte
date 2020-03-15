e941d38489459d22fc5b340e9c799c5b3c8430ec
AnjaliRagupathi <anjaliragupathi99@gmail.com>
2020-03-15 12:34:40 +0530

    created quiz card

---
+++ src/components/QuizCard.svelte
@@ -0,0 +1,46 @@
+<script>
+    export let quizName,  semester, courseCode, startTime, endTime, password;
+
+    const deleteQuiz = () => {
+        if (confirm("Are you sure you want to delete?")) {
+            db.collection("course-by-sem").doc(String(semester)).collection("courselist").doc(courseCode).collection("quizzes").doc(quizName).delete()
+                .then(doc => {
+                    console.log('Quiz deleted :(')
+                });
+        } else {
+            console.log("Öperation aborted")
+        }
+
+    }
+</script>
+
+<div class="card">
+    <header class="card-header">
+        <p class="card-header-title">
+            {'Quiz Name :' + quizName}
+        </p>
+    </header>
+    <div class="card-content">
+        <p>
+            {'Password: ' + password}
+        </p>
+        <p>
+            {'Start Time: ' + startTime}
+        </p>
+        <p>
+            {'End Time: ' + endTime}
+        </p>
+    </div>
+    <footer class="card-footer">
+        <a class="card-footer-item">Edit</a>
+        <a class="card-footer-item" on:click={deleteQuiz}>Delete</a>
+    </footer>
+</div>
+
+<style>
+    .card {
+        width: 100%;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+</style>
