6fd963a49888345ce09cb2e79cf4517c9759dd87
AnjaliRagupathi <anjaliragupathi99@gmail.com>
2020-04-25 21:52:34 +0530

    newQuiz UI test

--- cypress/integration/pageload.js
+++ cypress/integration/pageload.js
@@ -1,9 +1,16 @@
 
 
-describe('Loading the page',function(){
-    it('navigating to the page', function(){
+describe('Loading the New Quiz page',function(){
+    it('navigating to the index page', function(){
         cy.visit('/')
-    }
+    })
 
-    
+    it('clickbutton', function(){
+        cy.get('#newQuiz').click()   
+            
+    })
+
+    it('navigating to newQuiz',function(){
+        cy.visit('/newQuiz')
+    })
 })
